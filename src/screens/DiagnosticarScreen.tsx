3746b614ce1bad72cf17ac34a078bda6588843a8
Luis Arrieta <luisarrieta796@gmail.com>
2024-07-19 11:48:48 -0500

    Mejorar los estilos de la pantalla Diagnósticos

--- src/screens/DiagnosticarScreen.tsx
+++ src/screens/DiagnosticarScreen.tsx
@@ -1,20 +1,41 @@
-import {
-  Button,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {COLORS} from '../theme';
+import ButtonText from '../components/ButtonPresseable';
 export default function DiagnosticarScreen({navigation}: any) {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.heading}>
+      <View style={styles.decoration} />
+      <Text
+        style={[
+          styles.heading,
+          {color: COLORS.primary.actived, marginTop: 0, marginBottom: 20},
+        ]}>
         Diagnóstico de Enfermedades en Cultivos
       </Text>
+
       <Text style={styles.heading}>Diagnóstico de la Paratrioza</Text>
+      <View style={{marginBottom: 20}}>
+        <ScrollView horizontal showsVerticalScrollIndicator={false}>
+          <View style={{width: 20}} />
+          <Image
+            source={require('../assets/images/paratrioza_vector.jpg')}
+            style={styles.image}
+          />
+          <Image
+            source={require('../assets/images/paratrioza_huevos.jpg')}
+            style={styles.image}
+          />
+          <Image
+            source={require('../assets/images/pmp_tallos.jpg')}
+            style={styles.image}
+          />
+          <Image
+            source={require('../assets/images/pmp_hojas.jpg')}
+            style={styles.image}
+          />
+        </ScrollView>
+      </View>
       <Text style={styles.text}>
         La Punta Morada de la papa es una enfermedad compleja identificada
         inicialmente en Suramérica, afectando gravemente los cultivos de papa en
@@ -22,73 +43,70 @@ export default function DiagnosticarScreen({navigation}: any) {
         a través del insecto vector Bactericella cockerelli Sulc., también
         conocido como Paratrioza.
       </Text>
-      <View style={styles.imageGrid}>
-        <Image
-          source={require('../assets/images/paratrioza_vector.jpg')}
-          style={styles.image}
-        />
-        <Image
-          source={require('../assets/images/paratrioza_huevos.jpg')}
-          style={styles.image}
-        />
-        <Image
-          source={require('../assets/images/pmp_tallos.jpg')}
-          style={styles.image}
-        />
-        <Image
-          source={require('../assets/images/pmp_hojas.jpg')}
-          style={styles.image}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.button}
+      <ButtonText
+        style={{paddingHorizontal: 50, alignSelf: 'center'}}
         onPress={() => navigation.navigate('Paratrioza')}>
-        <Text style={styles.buttonText}>Diagnosticar</Text>
-      </TouchableOpacity>
+        Diagnosticar
+      </ButtonText>
+
       <Text style={styles.heading}>Diagnóstico de Tizón Tardío</Text>
+      <View style={{marginBottom: 20}}>
+        <ScrollView horizontal showsVerticalScrollIndicator={false}>
+          <View style={{width: 20}} />
+          <Image
+            source={require('../assets/images/gota.jpg')}
+            style={styles.image}
+          />
+          <Image
+            source={require('../assets/images/late_blight_329.jpg')}
+            style={styles.image}
+          />
+        </ScrollView>
+      </View>
       <Text style={styles.text}>
         El tizón tardío, causado por el hongo Phytophthora infestans, es una
         enfermedad devastadora que afecta principalmente a plantas de la familia
         de las solanáceas, como la papa y el tomate.
       </Text>
-      <View style={styles.imageGrid}>
-        <Image
-          source={require('../assets/images/gota.jpg')}
-          style={styles.image}
-        />
-        <Image
-          source={require('../assets/images/late_blight_329.jpg')}
-          style={styles.image}
-        />
-      </View>
-      <TouchableOpacity
-        style={styles.button}
+      <ButtonText
+        style={{paddingHorizontal: 50, alignSelf: 'center'}}
         onPress={() => navigation.navigate('Gota')}>
-        <Text style={styles.buttonText}>Diagnosticar</Text>
-      </TouchableOpacity>
+        Diagnosticar
+      </ButtonText>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    paddingHorizontal: 20,
     paddingVertical: 30,
-    alignItems: 'center',
+    backgroundColor: COLORS.primary.dark,
+  },
+  decoration: {
+    width: 500,
+    height: 300,
+    borderRadius: 200,
+    alignSelf: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    position: 'absolute',
+    top: -170,
   },
   heading: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 23,
-    color: 'black',
+    marginTop: 40,
+    marginBottom: 10,
+    paddingHorizontal: 20,
     textAlign: 'center',
+    color: 'white',
   },
   text: {
     fontSize: 16,
     lineHeight: 24,
     textAlign: 'center',
     marginBottom: 20,
+    paddingHorizontal: 20,
+    color: 'rgba(255,255,255, 0.8)',
   },
   imageGrid: {
     flexDirection: 'row',
@@ -99,19 +117,11 @@ const styles = StyleSheet.create({
     alignItems: 'baseline',
   },
   image: {
-    width: '50%',
-    height: 140,
-    marginBottom: 10,
+    width: 270,
+    height: 320,
     borderRadius: 10,
-  },
-  button: {
-    backgroundColor: '#4CAF50',
-    padding: 15,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    textAlign: 'center',
+    marginRight: 10,
+    borderColor: 'white',
+    borderWidth: 7,
   },
 });
